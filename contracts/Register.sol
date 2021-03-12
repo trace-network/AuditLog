cb7a7018b307f87343a6820f96bc68e25fc97b78
trace.sourcecode <80528472+trace-network@users.noreply.github.com>
2021-03-12 19:20:50 +0530

    Add files via upload

---
+++ contracts/Register.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.4.24;
+
+contract Register{
+    
+    event RegisterDevice(string data);
+    address public proxy = 0x74E2aa7E275AC6B38Fc6F46B8ac0Ab753c3Dd0FC;
+    function register(string data) public {
+        require(msg.sender == proxy);
+        emit RegisterDevice(data);
+    }
+}
